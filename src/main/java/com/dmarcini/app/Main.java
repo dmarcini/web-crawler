11bbea28266de2b04ec3c8bd29910ee9a5afc6f4
Damian Marciniak <damian.marciniak.1996@gmail.com>
2020-07-29 22:37:22 +0200

    Initialize Maven-JavaFX project

---
+++ src/main/java/com/dmarcini/app/Main.java
@@ -0,0 +1,25 @@
+package com.dmarcini.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        AnchorPane root = FXMLLoader.load(Main.class.getResource("views/web-crawler.fxml"));
+
+        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
