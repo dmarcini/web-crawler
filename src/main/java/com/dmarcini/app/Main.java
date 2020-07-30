2c43365cb90d04f3f1854d48f1c5e1b74e91aae4
Damian Marciniak <damian.marciniak.1996@gmail.com>
2020-07-30 14:45:26 +0200

    Add HTML reading from URL

--- src/main/java/com/dmarcini/app/Main.java
+++ src/main/java/com/dmarcini/app/Main.java
@@ -3,7 +3,7 @@ package com.dmarcini.app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +12,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        AnchorPane root = FXMLLoader.load(Main.class.getResource("views/web-crawler.fxml"));
+        Pane root = FXMLLoader.load(Main.class.getResource("views/web-crawler.fxml"));
 
-        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.setTitle("Web Crawler");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
--- src/main/java/com/dmarcini/app/controllers/WebCrawlerController.java
+++ src/main/java/com/dmarcini/app/controllers/WebCrawlerController.java
@@ -1,5 +1,68 @@
 package com.dmarcini.app.controllers;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class WebCrawlerController {
+    @FXML
+    private TextField URLTextField;
+    @FXML
+    private Button runButton;
+    @FXML
+    private TextArea HTMLTextArea;
+
+    public WebCrawlerController() { }
+
+    @FXML
+    private void initialize() {
+        runButton.setOnAction(actionEvent -> getHTML());
+
+        HTMLTextArea.setEditable(false);
+
+        Platform.runLater(() -> URLTextField.requestFocus());
+    }
+
+    @FXML
+    private void getHTML() {
+        HTMLTextArea.setText(readHTML(URLTextField.getText()));
+    }
+
+    private String readHTML(String URLAddress) {
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        try (final InputStream inputStream = new URL(URLAddress).openStream();
+             final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,
+                                                                                            StandardCharsets.UTF_8))) {
+             String nextLine;
+
+             while ((nextLine = bufferedReader.readLine()) != null) {
+                 stringBuilder.append(nextLine).append(System.lineSeparator());
+             }
+        } catch (IOException e) {
+            showIncorrectURLAlert();
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private void showIncorrectURLAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        
+        alert.setHeaderText("Incorrect URL");
+        alert.setContentText("Incorrect URL address. Please type again!");
+
+        alert.showAndWait();
+    }
 
 }
